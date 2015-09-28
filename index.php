b47441a6afd5e8493ea556cf933cda1d778ef0ae
unknown <2daw@winmac6.alumnado.flo>
2015-09-28 13:16:42 +0200

    Primer commit :D

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Mi primerita pagina web</title>
+	</head>
+	<body>
+			Mi primer codigo HTML
+			<br>
+			<?php 
+
+			echo "Hola mundo!";
+			echo "<br>";
+			echo "Ejemplo
+			sii";
+
+			 ?>
+	</body>
+</html>
